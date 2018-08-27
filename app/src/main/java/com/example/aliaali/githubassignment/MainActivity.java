1363309e9889cb41a89b9c611ff04040e87914a1
Ali A. Ali <aliabdolkarim98@gmail.com>
2018-08-27 19:11:48 +0300

    completed the project

--- app/src/main/java/com/example/aliaali/githubassignment/MainActivity.java
+++ app/src/main/java/com/example/aliaali/githubassignment/MainActivity.java
@@ -17,47 +17,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
 
-        binding.numOneImageButton.setOnClickListener(this);
-        binding.numTwoImageButton.setOnClickListener(this);
-        binding.numThreeImageButton.setOnClickListener(this);
-        binding.numFourImageButton.setOnClickListener(this);
-        binding.numFiveImageButton.setOnClickListener(this);
-        binding.numSixImageButton.setOnClickListener(this);
-        binding.numSevenImageButton.setOnClickListener(this);
-        binding.numEightImageButton.setOnClickListener(this);
-        binding.numNineImageButton.setOnClickListener(this);
+//        binding.numOneImageButton.setOnClickListener(this);
+//        binding.numTwoImageButton.setOnClickListener(this);
+//        binding.numThreeImageButton.setOnClickListener(this);
+//        binding.numFourImageButton.setOnClickListener(this);
+//        binding.numFiveImageButton.setOnClickListener(this);
+//        binding.numSixImageButton.setOnClickListener(this);
+//        binding.numSevenImageButton.setOnClickListener(this);
+//        binding.numEightImageButton.setOnClickListener(this);
+//        binding.numNineImageButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.num_one_image_button:
-               MediaPlayer.create(this,R.raw.one).start();
-                break;
-           case R.id.num_two_image_button:
-               MediaPlayer.create(this,R.raw.two).start();
-                break;
-           case R.id.num_three_image_button:
-               MediaPlayer.create(this,R.raw.three).start();
-                break;
-           case R.id.num_four_image_button:
-               MediaPlayer.create(this,R.raw.four).start();
-                break;
-           case R.id.num_five_image_button:
-               MediaPlayer.create(this,R.raw.five).start();
-                break;
-           case R.id.num_six_image_button:
-               MediaPlayer.create(this,R.raw.six).start();
-                break;
-           case R.id.num_seven_image_button:
-               MediaPlayer.create(this,R.raw.seven).start();
-                break;
-           case R.id.num_eight_image_button:
-               MediaPlayer.create(this,R.raw.eight).start();
-                break;
-           case R.id.num_nine_image_button:
-               MediaPlayer.create(this,R.raw.nine).start();
-                break;
+//            case R.id.num_one_image_button:
+//               MediaPlayer.create(this,R.raw.one).start();
+//                break;
+//           case R.id.num_two_image_button:
+//               MediaPlayer.create(this,R.raw.two).start();
+//                break;
+//           case R.id.num_three_image_button:
+//               MediaPlayer.create(this,R.raw.three).start();
+//                break;
+//           case R.id.num_four_image_button:
+//               MediaPlayer.create(this,R.raw.four).start();
+//                break;
+//           case R.id.num_five_image_button:
+//               MediaPlayer.create(this,R.raw.five).start();
+//                break;
+//           case R.id.num_six_image_button:
+//               MediaPlayer.create(this,R.raw.six).start();
+//                break;
+//           case R.id.num_seven_image_button:
+//               MediaPlayer.create(this,R.raw.seven).start();
+//                break;
+//           case R.id.num_eight_image_button:
+//               MediaPlayer.create(this,R.raw.eight).start();
+//                break;
+//           case R.id.num_nine_image_button:
+//               MediaPlayer.create(this,R.raw.nine).start();
+//                break;
         }
     }
 }
