045f7b8182a1759e4735f88adb0296ee1e4e855f
Ali A. Ali <aliabdolkarim98@gmail.com>
2018-08-27 16:49:05 +0300

    completed the project

--- app/src/main/java/com/example/aliaali/githubassignment/MainActivity.java
+++ app/src/main/java/com/example/aliaali/githubassignment/MainActivity.java
@@ -10,13 +10,11 @@ import android.widget.ImageButton;
 import com.example.aliaali.githubassignment.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private MediaPlayer mediaPlayer;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
 
         binding.numOneImageButton.setOnClickListener(this);
@@ -28,20 +26,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding.numSevenImageButton.setOnClickListener(this);
         binding.numEightImageButton.setOnClickListener(this);
         binding.numNineImageButton.setOnClickListener(this);
-        binding.numZeroImageButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-//        switch (v.getId()){
-//            case binding.numOneImageButton:
-//                mediaPlayer=MediaPlayer.create(this,)
-//            case binding.
-//            case binding.
-//            case binding.
-//            case binding.
-//            case binding.
-//            case binding.
-//        }
+        switch (v.getId()){
+            case R.id.num_one_image_button:
+               MediaPlayer.create(this,R.raw.one).start();
+                break;
+           case R.id.num_two_image_button:
+               MediaPlayer.create(this,R.raw.two).start();
+                break;
+           case R.id.num_three_image_button:
+               MediaPlayer.create(this,R.raw.three).start();
+                break;
+           case R.id.num_four_image_button:
+               MediaPlayer.create(this,R.raw.four).start();
+                break;
+           case R.id.num_five_image_button:
+               MediaPlayer.create(this,R.raw.five).start();
+                break;
+           case R.id.num_six_image_button:
+               MediaPlayer.create(this,R.raw.six).start();
+                break;
+           case R.id.num_seven_image_button:
+               MediaPlayer.create(this,R.raw.seven).start();
+                break;
+           case R.id.num_eight_image_button:
+               MediaPlayer.create(this,R.raw.eight).start();
+                break;
+           case R.id.num_nine_image_button:
+               MediaPlayer.create(this,R.raw.nine).start();
+                break;
+        }
     }
 }
